4fff0890c0e411f165e02f8c290256b2c34a8c66
ntchaudhary <37553212+ntchaudhary@users.noreply.github.com>
2022-11-08 23:53:44 +0530

    added visual part for salary entry

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
+import threading
+from tkinter import messagebox
 
 from openpyxl import load_workbook
 from datetime import date
 
-from openpyxl.chart import PieChart, Reference
-from openpyxl.chart.label import DataLabelList
+from tkinter import *
+
+root = Tk()
+root.geometry('250x100')
+root.iconbitmap(r'MC.ico')
+root.resizable(False, False)
+
+__salary = StringVar()
 
 wb = load_workbook('MoneyControl.xlsx')
 wb.iso_dates = True
 ws = wb.active
 
 MAX_ROW = ws.max_row
+
 FORMULAS = [
     date.today().strftime('%d-%m-%Y'),
     f'=(A{MAX_ROW + 1}-A{MAX_ROW})',
@@ -18,50 +27,42 @@ FORMULAS = [
     f'=ROUNDDOWN((E{MAX_ROW}+(C{MAX_ROW + 1}*0.2)),0)',
     f'=ROUNDDOWN((F{MAX_ROW}+(C{MAX_ROW + 1}*0.3)),0)',
     f'=ROUNDUP((G{MAX_ROW}+(C{MAX_ROW + 1}*0.5)),0)',
-    '',
-    f'=ROUNDUP(G{MAX_ROW+1}*0.6,0)',
-    f'=ROUNDDOWN(G{MAX_ROW+1}*0.4,0)',
-    '',
-    f'=ROUNDUP(I{MAX_ROW+1}*0.4,0)',
-    f'=ROUNDUP(I{MAX_ROW+1}*0.25,0)',
-    f'=ROUNDDOWN(I{MAX_ROW+1}*0.2,0)',
-    f'=ROUNDDOWN(I{MAX_ROW+1}*0.15,0)',
+    'black',
+    f'=ROUNDUP(G{MAX_ROW + 1}*0.6,0)',
+    f'=ROUNDDOWN(G{MAX_ROW + 1}*0.4,0)',
+    'black',
+    f'=ROUNDUP(I{MAX_ROW + 1}*0.4,0)',
+    f'=ROUNDUP(I{MAX_ROW + 1}*0.25,0)',
+    f'=ROUNDDOWN(I{MAX_ROW + 1}*0.2,0)',
+    f'=ROUNDDOWN(I{MAX_ROW + 1}*0.15,0)',
 ]
 
-dataToBeAdded = [int(input()), ]
-dataToBeAdded = dataToBeAdded + FORMULAS
 
-print(dataToBeAdded)
+def submit():
+    inputData = __salary.get()
+    if not inputData.isdigit():
+        messagebox.showerror("Error", "Please enter valid in-hand salary")
+        entry.delete(0, 'end')
+        return
 
-ws.append(dataToBeAdded)
+    dataToBeAdded = [int(inputData), ]
+    dataToBeAdded = dataToBeAdded + FORMULAS
 
-wb.save('MoneyControl.xlsx')
+    print(dataToBeAdded)
 
-wb.close()
+    ws.append(dataToBeAdded)
+
+    wb.save('MoneyControl.xlsx')
+
+    wb.close()
+    root.destroy()
+
+    os.startfile('MoneyControl.xlsx')
 
 
-# plotting Pie Chart for the current salary breakup
-wb = load_workbook('MoneyControl.xlsx')
-ws = wb.active
-pie = PieChart()
+Label(root, text="Enter in-hand salary").pack(side='top')
+entry = Entry(root, textvariable=__salary, justify='center', bd=3, font='18')
+entry.pack(expand=True, fill='both', side='top')
+Button(root, text='Submit', command=submit).pack()
 
-# selecting labels for the chart
-labels = Reference(ws, min_row=1, max_row=1, min_col=5, max_col=7)
-
-# selecting data for the chart
-data = Reference(ws, min_row=ws.max_row, max_row=ws.max_row, min_col=4, max_col=7)
-
-pie.add_data(data=data, from_rows=True, titles_from_data=True)
-pie.set_categories(labels)
-
-# Showing data labels as percentage
-pie.dataLabels = DataLabelList()
-pie.dataLabels.showPercent = True
-
-pie.title = 'Salary Breakup'
-
-ws.add_chart(pie, 'Q5')
-wb.save('MoneyControl.xlsx')
-wb.close()
-
-os.system('MoneyControl.xlsx')
+root.mainloop()
